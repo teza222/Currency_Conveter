b4b95f612585c82ed5d3ddbeae4e0abc2869abde
Everton's iMac <evertonrichards0@gmail.com>
2025-03-08 23:14:36 -0500

    Completed

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+
+        double usdValue = 129.02;
+        double canValue = 97.50;
+        double euroValue = 164.33;
+        final double[] convertedAmount = {0};
+
+        final String[] initCurrency = {"US"};
 
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
@@ -22,6 +32,8 @@ public class Main {
 
         panel.setLayout(null);
 
+
+
         JLabel inputLabel = new JLabel("Input $:");
         inputLabel.setBounds(10,20,165,35);
         panel.add(inputLabel);
@@ -36,16 +48,20 @@ public class Main {
 
         String[] currencyCode = {"US","CAN","Euro"};
 
-        JComboBox currencyTextField = new JComboBox(currencyCode);
-        currencyTextField.setSelectedIndex(2);
-        currencyTextField.addActionListener(new ActionListener() {
+        JComboBox currencyBox = new JComboBox(currencyCode);
+        currencyBox.addItemListener(new ItemListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    initCurrency[0] = (String)currencyBox.getSelectedItem();
 
+                    System.out.print(initCurrency[0]);
+
+                }
             }
         });
-        currencyTextField.setBounds(120,50,165,35);
-        panel.add(currencyTextField);
+        currencyBox.setBounds(120,50,165,35);
+        panel.add(currencyBox);
 
         JLabel outputLabel = new JLabel("JMD Amount $:");
         outputLabel.setBounds(10,80,165,35);
@@ -58,20 +74,48 @@ public class Main {
 
 
         JButton convertBtn = new JButton("Convert");
-        convertBtn.setBounds(60, 120,100,25);
+        convertBtn.setBounds(60, 120,100,30);
         convertBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String getAmount = inputTextField.getText();
+                double inputCurrency2Double = 0;
+                try {
+                    inputCurrency2Double = Double.parseDouble(getAmount);
+                }catch (NumberFormatException ignored){
+                    System.out.println("Cannot convert a null value");
+                    //catches if string is empty or non-numerical value entered
+                    JOptionPane.showMessageDialog(null,"Please input a valid numerical value!");
+                }
+
+
+
+                switch (initCurrency[0]){
+                    case "US": convertedAmount[0] = inputCurrency2Double * usdValue;
+                        break;
+                    case "CAN": convertedAmount[0] = inputCurrency2Double * canValue;
+                        break;
+                    case "Euro": convertedAmount[0] = inputCurrency2Double * euroValue;
+                        break;
+                }
+
+                outPutTextField.setText(String.format( "%.2f", convertedAmount[0] ));
+
 
             }
         });
         panel.add(convertBtn);
 
         JButton clearBtn = new JButton("Clear");
-        clearBtn.setBounds(160, 120,100,25);
+        clearBtn.setBounds(160, 120,100,30);
         clearBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                inputTextField.setText("");
+                outPutTextField.setText("");
+                currencyBox.setSelectedIndex(0);
+                initCurrency[0] = "US";
 
             }
         });
